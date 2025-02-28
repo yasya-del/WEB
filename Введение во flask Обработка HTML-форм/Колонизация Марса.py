7a0c50b11dadc596ad62f9ed0ceaeb7e0cbb6975
yasya <vasiliszaitseva@yandex.ru>
2025-02-28 18:05:32 +0300

    урок 1 задача Варианты выбора

--- Введение во flask Обработка HTML-форм/Колонизация Марса.py
+++ Введение во flask Обработка HTML-форм/Колонизация Марса.py
@@ -67,5 +67,36 @@ def promotion_image():
                         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                       </body>
                     </html>"""
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <title>Варианты выбора</title>
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                          </head>
+                          <body>
+                            <h1>Мое предложение: {planet_name}</h1>
+                            <h2>Эта планета близка к Земле;<h2>
+                            <div class="alert alert-danger" role="alert">
+                            Она самая классная!
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                            Самая крутая!
+                            </div>
+                            <div class="alert alert-success" role="alert">
+                            И находится во Вселенной!
+                            </div>
+                            <div class="alert alert-info" role="alert">
+                            Короче, самая лучшая!@
+                            </div>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                          </body>
+                        </html>"""
+@app.route('/astronaut_selection')
+def astronaut_selection():
+    pass
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
