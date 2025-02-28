5ac8d54bb05b1689642ba0e61afbb9dc5cb83238
yasya <vasiliszaitseva@yandex.ru>
2025-02-28 17:54:54 +0300

    урок 1 задача Реклама с картинкой

--- Введение во flask Обработка HTML-форм/Колонизация Марса.py
+++ Введение во flask Обработка HTML-форм/Колонизация Марса.py
@@ -24,13 +24,48 @@ def return_sample_page():
                   <head>
                     <meta charset="utf-8">
                     <title>Привет Марс!</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/MARS.png')}" 
            alt="здесь должна была быть картинка, но не нашлась">
                     <p> Вот она какая, красная планета.</p>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                   </body>
                 </html>"""
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Колонизация</title>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/MARS.png')}" 
+               alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-danger" role="alert">
+                        Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                        Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-info" role="alert">
+                        И начнем с Марса!
+                        </div>
+                        <div class="alert alert-primary" role="alert">
+                        Присоединяйся!
+                        </div>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                      </body>
+                    </html>"""
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
