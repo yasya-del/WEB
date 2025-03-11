6591e73c5aa2fa6bb61c6eca34e36e4a692cefc3
yasya <vasiliszaitseva@yandex.ru>
2025-03-11 18:13:31 +0300

    урок 2 Список профессий

--- WEB Шаблоны flask-wtf/Готовимся к миссии.py
+++ WEB Шаблоны flask-wtf/Готовимся к миссии.py
@@ -8,5 +8,8 @@ def index(title):
 @app.route('/training/<prof>')
 def trainy(prof):
     return render_template('training.html', prof=prof)
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_prof.html', list=list)
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
